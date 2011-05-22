b3a0a5800f1bf3c96c99228507f87530a64031a9
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-05-23 00:46:34 +0300

    Sketch of the implementation, registration works, tons of work ahead.

--- bin/gomoku-client-cli.py
+++ bin/gomoku-client-cli.py
@@ -11,45 +11,109 @@ from __future__ import print_function
 
 
 import argparse
-import socket
 import simplejson
-import struct
+from gomoku import utils
+from twisted.internet import reactor
+from twisted.internet.protocol import ReconnectingClientFactory
+from twisted.protocols.basic import Int32StringReceiver
 
 
 
-HEADER = struct.calcsize('!I')
+INITED = 0
+AUTHENTICATED = 1
 
 
 
-def read(sock):
-    header = sock.recv(HEADER)
-    length = struct.unpack('!I', header)[0]
-    body = sock.recv(length)
-    return simplejson.loads(body)
+class GomokuClientProtocol(Int32StringReceiver):
+
+
+    factory = None
+
+
+    def __init__(self):
+        self.dispatch = {utils.AUTH_OK: self.authenticated,
+                         utils.REG_OK: self.registered,
+                         utils.AUTH_ERR_PASSWORD: self.wrongpass,
+                         utils.AUTH_ERR_NOTFOUND: self.notauser}
+
+
+    def connectionMade(self):
+        if self.factory.state != AUTHENTICATED:
+            self.auth()
+
+
+    def authenticated(self, response):
+        self.factory.state = AUTHENTICATED
+
+
+    def registered(self, response):
+        print('You have been registered!')
+        self.factory.state = AUTHENTICATED
+
+
+    def wrongpass(self, response):
+        print('Password mismatch!')
+        self.auth()
+
+
+    def notauser(self, response):
+        print('There is no such user. Registration required')
+        while True:
+            password = self.password()
+            if password == self.password('Re-type your password: '):
+                break
+            print('Oops, the two password have to match. Try again')
+        self.send({'action': utils.REGISTER,
+                   'user': self.factory.user,
+                   'password': password})
 
 
 
-def write(sock, data):
-    body = simplejson.dumps(data)
-    length = len(body)
-    header = struct.pack('!I', length)
-    sock.send(header + body)
+    def password(self, message="Type your password: "):
+        password = None
+        while not password:
+            password = raw_input(message)
+        return password
+
+
+    def auth(self):
+        password = self.password()
+        self.send({'action': utils.AUTH,
+                   'user': self.factory.user,
+                   'password': password})
+
+
+    def stringReceived(self, response):
+        response = simplejson.loads(response)
+        self.dispatch[response['action']](response)
+
+
+    def send(self, request):
+        request = simplejson.dumps(request)
+        self.sendString(request)
 
 
 
-def greet(sock):
-    name = raw_input("Type in your name: ")
-    write(sock, {'action': 'greet', 'name': name})
-    return read(sock)
+class GomokuClientFactory(ReconnectingClientFactory):
 
+
+    protocol = GomokuClientProtocol
+
+
+    def __init__(self):
+        self.state = INITED
+        self.user = None
+
+
+    def startFactory(self):
+        while not self.user:
+            self.user = raw_input('Name: ')
 
 
 def main(args):
-    sock = socket.create_connection((args.host, args.port))
-    greet(sock)
-    while True:
-        act(read(sock))
-        move(sock)
+    factory = GomokuClientFactory()
+    reactor.connectTCP(args.host, args.port, factory)
+    reactor.run()
 
 
 if __name__ == '__main__':
--- bin/gomoku-server.py
+++ bin/gomoku-server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import with_statement
+
 """
 Server that runs gomoku game.
 
@@ -12,9 +14,17 @@ It should serve several purposes:
 
 import argparse
 import simplejson
+from gomoku import utils
+import anydbm
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import Int32StringReceiver
+
+
+# just a set of constants which determine the state of the connection
+CONNECTED = 0
+DONE = -1
+AUTHENTICATED = 1
 
 
 
@@ -23,10 +33,51 @@ class GomokuProtocol(Int32StringReceiver):
     """Each client will have one of these"""
 
 
-    def stringReceived(self, string):
-        string = simplejson.loads(string)
+    factory = None
 
-        self.sendString(simplejson.dumps(string))
+
+    def __init__(self):
+        self.user = None
+        self.state = CONNECTED
+        self.dispatch = {utils.AUTH: self.auth,
+                         utils.REGISTER: self.register}
+
+
+    def send(self, response):
+        response = simplejson.dumps(response)
+        self.sendString(response)
+
+
+    def stringReceived(self, string):
+        request = simplejson.loads(string)
+        try:
+            self.dispatch[request['action']](request)
+        except KeyError:
+            self.send({'action': utils.NOTIMPLEMENTED})
+
+
+    def auth(self, request):
+        try:
+            if self.factory.db[request['user']] == request['password']:
+                self.state = AUTHENTICATED
+                self.send({'action': utils.AUTH_OK})
+            else:
+                self.send({'action': utils.AUTH_ERR_PASSWORD})
+        except KeyError:
+            self.send({'action': utils.AUTH_ERR_NOTFOUND})
+
+
+    def register(self, request):
+        try:
+            if request['user'] in self.factory.db:
+                # This is bad, we shouldn't allow someone overwriting already
+                # existing users
+                self.send({'action': utils.BADREQUEST})
+            else:
+                self.factory.db[request['user']] = request['password']
+                self.send({'action': utils.REG_OK})
+        except KeyError:
+            self.send({'action': utils.AUTH_ERR_NOTFOUND})
 
 
 
@@ -37,10 +88,22 @@ class GomokuFactory(Factory):
     protocol = GomokuProtocol
 
 
+    def __init__(self, dbpath):
+        self.dbpath = dbpath
+        self.db = None
+
+
+    def startFactory(self):
+        self.db = anydbm.open(self.dbpath, 'c')
+
+
+    def stopFactory(self):
+        self.db.close()
+
 
 
 def main(args):
-    factory = GomokuFactory()
+    factory = GomokuFactory(args.db_path)
     reactor.listenTCP(args.port, factory)
     reactor.run()
 
@@ -49,4 +112,5 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', default=5672, type=int)
+    parser.add_argument('-d', '--db-path', default='gomoku.db')
     main(parser.parse_args())
